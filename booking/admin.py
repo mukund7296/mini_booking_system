a771268d1ce8276fbcf67ca4a7d0b0e87916e1b4
Mukund Biradar <mukund2015@outlook.com>
2025-03-24 19:54:29 +0100

    Update admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Facility, Booking
 
 # Register your models here.
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'capacity')
+    search_fields = ('name', 'location')
+    list_filter = ('location',)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'facility', 'date', 'status')
+    list_filter = ('facility', 'date', 'status', 'user')
+    search_fields = ('user__username', 'facility__name')  # Search by user's username and facility name
+    date_hierarchy = 'date'
